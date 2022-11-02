ff464350a8ea94911aecd07f683cb0c225c1debf
byKriz <cristianesquivelh@hotmail.com>
2022-11-02 18:21:44 -0400

    agregando los correspondientes links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,10 @@ import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 export const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <a href="#" className={styles.link}>
+      <a href="https://github.com/byKriz/ReactMovieApp" className={styles.link} target='_blank'>
         <AiFillGithub />
       </a>
-      <a href="#" className={styles.link}>
+      <a href="https://www.linkedin.com/in/cristian-esquivel-351613135/" className={styles.link} target='_blank'>
         <AiFillLinkedin />
       </a>
     </footer>
