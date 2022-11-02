40a8cb3c52b44e56fe8dfd85b50785b3518efdf6
byKriz <cristianesquivelh@hotmail.com>
2022-11-01 21:04:07 -0400

    limpiando estilos

---
+++ src/components/MovieItem/MovieItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./MovieItem.module.scss";
+
+export const MovieIteam = ({data}) => {
+
+  return (
+    <div className={styles.Movie}>
+      Movie componente
+    </div>
+  );
+};
