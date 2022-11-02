4d5ae6591a7b492a686eba4c847c66b7c0fef977
byKriz <cristianesquivelh@hotmail.com>
2022-11-01 21:03:41 -0400

    agregando config de la api

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+export const apiConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    feature: import.meta.env.VITE_FEATURE,
+    images: import.meta.env.VITE_IMAGES,
+    search: import.meta.env.VITE_SEARCH,
+}
+
+console.log(apiConfig.apiKey)
