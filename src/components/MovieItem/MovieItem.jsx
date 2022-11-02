c58931976cdce490a7c50afe04c165e934161caf
byKriz <cristianesquivelh@hotmail.com>
2022-11-02 01:39:02 -0400

    agregando los estilos iniciales

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./MovieItem.module.scss";
+import { apiConfig } from "../../api/apiConfig";
 
-export const MovieIteam = ({data}) => {
+export const MovieIteam = ({ data }) => {
+  console.log(data);
 
   return (
-    <div className={styles.Movie}>
-      Movie componente
+    <div className={styles.Movie_item}>
+      <img src={`${apiConfig.images}${data.poster_path}`} alt={data.title} />
+      <div className={styles.movie_info}>
+        <h3>{data.title}</h3>
+        <span>{data.vote_average}</span>
+      </div>
     </div>
   );
 };
